48b71849611372b3194f1f64777e768f43c9cbb3
sourserge <15277406+souserge@users.noreply.github.com>
2020-02-16 22:25:39 +0200

    Complete the mock app

--- src/components/MockedApp.vue
+++ src/components/MockedApp.vue
@@ -2,7 +2,17 @@
   <div class="container">
     <div class="mocked-app">
       <div class="menu" @click="toggleMenu"></div>
-      <transition name="slide">
+      <div class="order" @click="makeOrder">
+        <p>ORDER</p>
+        <!-- <div class="logo"></div> -->
+      </div>
+      <transition name="slide-right">
+        <div v-show="isOrderComplete" class="order-complete">
+          <p>{{ getOrderCompleteString() }}</p>
+          <p class="order-complete__greeting">Bon Appétit!</p>
+        </div>
+      </transition>
+      <transition name="slide-left">
         <div v-show="isMenuOpened" class="menu-dropdown">
           <ul>
             <li
@@ -24,7 +34,31 @@
       ></vue-displacement-slideshow>
       <div class="info">
         <div class="info__name">({{ currentDishIdx + 1 }}/{{ numDishes }}) {{ currentDish.name }}</div>
-        <div class="info__desc">{{ currentDish.desc }}</div>
+        <div class="info__size">
+          Portion size ({{currentDish.minSize}}–{{currentDish.maxSize}} grams):
+          <input
+            type="number"
+            :min="currentDish.minSize"
+            :max="currentDish.maxSize"
+            v-model="dishSizeSelector"
+            :step="10"
+          /> g
+        </div>
+        <!-- <div class="info__desc">{{ currentDish.desc }}</div> -->
+        <div class="info__ingredients">
+          <v-dropdown>
+            <template v-slot:label>Ingredients</template>
+            <template v-slot:content>{{ getIngredientsString(currentDish) }}</template>
+          </v-dropdown>
+        </div>
+        <div class="info__nutrition">
+          <v-dropdown>
+            <template v-slot:label>Nutrition</template>
+            <template v-slot:content>
+              <div class="nutrition-data">{{ getNutritionString(currentDish) }}</div>
+            </template>
+          </v-dropdown>
+        </div>
       </div>
       <div class="arrow arrow--left" @click="changeDishLeft"></div>
       <div class="arrow arrow--right" @click="changeDishRight"></div>
@@ -34,6 +68,7 @@
 
 <script>
 import VueDisplacementSlideshow from "vue-displacement-slideshow";
+import VDropdown from "./VDropdown";
 
 // function constrain(x, a, b) {
 //   return x < a ? a : x > b ? b : x;
@@ -45,7 +80,8 @@ function rollOver(x, a, b) {
 
 export default {
   components: {
-    VueDisplacementSlideshow
+    VueDisplacementSlideshow,
+    VDropdown
   },
   data() {
     return {
@@ -53,17 +89,57 @@ export default {
         {
           src: require("@/assets/dish-1.jpg"),
           name: "Rice with Salmon and veggies",
-          desc: "This is a beautiful dish"
+          desc: "This is a beautiful dish",
+          standardSize: 300,
+          selectedSize: 300,
+          minSize: 150,
+          maxSize: 600,
+          ingredients: [
+            { text: "Rice" },
+            { text: "salmon" },
+            { text: "soybeans" },
+            { text: "carrot" },
+            { text: "lemon" }
+          ],
+          nutrition: {
+            fat: 10,
+            saturatedFat: 2,
+            carbs: 30,
+            sugar: 5,
+            protein: 10,
+            fiber: 5
+          }
         },
         {
           src: require("@/assets/dish-2.jpg"),
           name: "Vegan choco pudding with fruit",
-          desc: "This one is even better!"
+          desc: "This one is even better!",
+          standardSize: 200,
+          selectedSize: 200,
+          minSize: 100,
+          maxSize: 300,
+          ingredients: [
+            { text: "Soya pudding" },
+            { text: "cocoa" },
+            { text: "banana slices" },
+            { text: "strawberry" },
+            { text: "nuts" }
+          ],
+          nutrition: {
+            fat: 10,
+            saturatedFat: 1,
+            carbs: 40,
+            sugar: 8,
+            protein: 20,
+            fiber: 15
+          }
         }
       ],
 
       currentDishIdx: 0,
-      isMenuOpened: false
+      isMenuOpened: false,
+      isOrderComplete: false,
+      dishSizeSelector: null
     };
   },
 
@@ -83,17 +159,15 @@ export default {
 
   methods: {
     setImage(idx) {
-      const cIdx = rollOver(idx, 0, this.numDishes - 1);
-      this.currentDishIdx = cIdx;
-      this.$refs.slideshow.next(cIdx);
+      this.$refs.slideshow.next(idx);
     },
 
     changeDishLeft() {
-      this.setImage(this.currentDishIdx - 1);
+      this.selectMenuDish(this.currentDishIdx - 1);
     },
 
     changeDishRight() {
-      this.setImage(this.currentDishIdx + 1);
+      this.selectMenuDish(this.currentDishIdx + 1);
     },
 
     toggleMenu() {
@@ -105,9 +179,42 @@ export default {
     },
 
     selectMenuDish(idx) {
-      this.setImage(idx);
+      const cIdx = rollOver(idx, 0, this.numDishes - 1);
+      this.currentDishIdx = cIdx;
+      this.dishSizeSelector = this.dishes[cIdx].selectedSize;
+      this.setImage(cIdx);
       this.closeMenu();
+    },
+
+    getIngredientsString(dish) {
+      return dish.ingredients.map(x => x.text).join(", ") + ".";
+    },
+
+    getNutritionString({ nutrition }) {
+      const cn = x => Math.round((x / 100) * this.dishSizeSelector);
+      return `Fat......................${cn(nutrition.fat)} g
+  of which saturated.....${cn(nutrition.saturatedFat)} g
+Carbs....................${cn(nutrition.carbs)} g
+  of which sugar.........${cn(nutrition.sugar)} g
+Protein..................${cn(nutrition.protein)} g
+Fiber....................${cn(nutrition.fiber)} g`;
+    },
+
+    makeOrder() {
+      this.isOrderComplete = true;
+      setTimeout(() => {
+        this.isOrderComplete = false;
+      }, 6000);
+    },
+
+    getOrderCompleteString() {
+      const price = "1569";
+      return `Ordered ${this.dishSizeSelector} grams of "${this.currentDish.name}" for €${price}.`;
     }
+  },
+
+  mounted() {
+    this.dishSizeSelector = this.currentDish.selectedSize;
   }
 };
 </script>
@@ -115,6 +222,7 @@ export default {
 <style lang="scss" scoped>
 .container {
   width: 100%;
+  background: black;
 }
 
 .mocked-app {
@@ -125,6 +233,7 @@ export default {
   height: 100vh;
   margin: 0 auto;
   position: relative;
+  overflow: hidden;
 
   .dish {
     width: 100%;
@@ -133,15 +242,15 @@ export default {
   }
 
   .info {
+    z-index: 9;
     position: absolute;
     width: 90%;
-    // height: 15%;
-    // min-height: 50px;
-    bottom: 1%;
+    bottom: 0;
     padding: 8px;
-    background: rgba(0, 0, 0, 0.5);
-    border: black solid 2px;
-    border-radius: 25px;
+    background: rgba(0, 0, 0, 0.66);
+    border: black solid;
+    border-width: 2px 2px 0 2px;
+    border-radius: 25px 25px 0 0;
     right: 50%;
     transform: translateX(50%);
     text-align: center;
@@ -155,37 +264,72 @@ export default {
       padding: 10px;
       text-align: left;
     }
+
+    &__size {
+      padding-top: 5px;
+
+      input {
+        font-size: 1.2em;
+        width: 50px;
+        color: white;
+        border: 0;
+        outline: 0;
+        background: transparent;
+        border-bottom: 1px solid white;
+      }
+    }
+
+    &__size,
+    &__ingredients,
+    &__nutrition {
+      margin-bottom: 7px;
+    }
+
+    &__nutrition {
+      .nutrition-data {
+        // text-align: center;
+        white-space: pre;
+        font-family: monospace;
+        font-weight: 600;
+        line-height: 1em;
+        font-size: 1.2em;
+      }
+    }
   }
 
   .arrow {
     background-image: url("../assets/arrow.svg");
     background-repeat: no-repeat;
     background-position: center;
-    background-size: contain;
+    background-size: 50px 50px;
     cursor: pointer;
-    width: 50px;
-    height: 50px;
+    width: 60px;
+    height: 50%;
     position: absolute;
-    top: calc(50% - 20px);
+    top: calc(50%);
 
     &--right {
-      right: 2%;
+      right: 0;
+      transform: translateY(-50%);
     }
 
     &--left {
-      left: 2%;
-      transform: rotate(180deg);
+      left: 0;
+      transform: translateY(-50%) rotate(180deg);
     }
 
     transition-property: filter;
     transition-duration: 0.2s;
 
     &:hover {
-      filter: drop-shadow(0 0 10px rgba(0, 0, 0, 0.5));
+      background-color: rgba(lightgrey, 0.3);
+
+      // filter: drop-shadow(0 0 10px rgba(0, 0, 0, 0.5));
     }
   }
 
   .menu {
+    z-index: 11;
     background-image: url("../assets/alacarte.svg");
     background-repeat: no-repeat;
     background-position: center;
@@ -206,11 +350,12 @@ export default {
   }
 
   .menu-dropdown {
+    z-index: 10;
     padding: 20px 10px;
     position: absolute;
-    top: 70px;
+    top: 80px;
     left: 0;
-    background: rgba(0, 0, 0, 0.5);
+    background: rgba(0, 0, 0, 0.66);
     border: black solid;
     border-width: 2px 2px 2px 0;
     border-radius: 0 25px 25px 0;
@@ -225,16 +370,72 @@ export default {
       }
     }
   }
+
+  .order {
+    z-index: 11;
+    position: absolute;
+    top: 1%;
+    right: 2%;
+    vertical-align: center;
+    text-align: center;
+    color: white;
+    font-size: 1em;
+    font-weight: 900;
+    width: 60px;
+    height: 60px;
+    border: 5px solid black;
+    background-color: rgba(0, 0, 0, 0.66);
+    border-radius: 100%;
+    cursor: pointer;
+
+    line-height: 60px;
+
+    transition-property: filter;
+    transition-duration: 0.2s;
+
+    &:hover {
+      filter: drop-shadow(2px 2px grey);
+    }
+  }
+
+  .order-complete {
+    z-index: 12;
+    padding: 20px 10px;
+    text-align: left;
+    background: rgba(0, 0, 0, 0.66);
+    border: black solid;
+    border-width: 2px 0 2px 2px;
+    border-radius: 25px 0 0 25px;
+    position: absolute;
+    color: white;
+    top: 80px;
+    left: 0;
+    margin-left: 20%;
+    font-weight: bold;
+    line-height: 1.1em;
+
+    &__greeting {
+      padding-top: 7px;
+      text-align: right;
+    }
+  }
 }
 
-.slide-enter-active {
+.slide-left-enter-active,
+.slide-right-enter-active {
   transition: all 0.5s ease;
 }
-.slide-leave-active {
+.slide-left-leave-active,
+.slide-right-leave-active {
   transition: all 0.5s cubic-bezier(1, 0.5, 0.8, 1);
 }
-.slide-enter,
-.slide-leave-to {
+.slide-left-enter,
+.slide-left-leave-to {
   transform: translateX(-100%);
+}
+
+.slide-right-enter,
+.slide-right-leave-to {
+  transform: translateX(100%);
 }
 </style>
---
+++ src/components/VDropdown.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="dropdown" :class="isOpened ? 'dropdown--opened' : ''">
+    <div class="dropdown__label" @click="toggle">
+      <slot name="label"></slot>
+      <div class="dropdown__icon"></div>
+    </div>
+    <div class="dropdown__content">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpened: false
+    };
+  },
+
+  methods: {
+    toggle() {
+      this.isOpened = !this.isOpened;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.dropdown {
+  text-align: left;
+  &__label {
+    font-size: 1.3em;
+    font-weight: bold;
+    display: inline-block;
+    margin-bottom: 0.3em;
+
+    &:hover {
+      background-color: rgba(lightgrey, 0.3);
+      cursor: pointer;
+    }
+  }
+
+  &__icon {
+    display: inline-block;
+    background: url("../assets/triangle.svg");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+    height: 1rem;
+    width: 1rem;
+    transition-property: transform;
+    transition-duration: 0.3s;
+    transform: rotate(180deg) translateY(-0.1rem);
+  }
+
+  &__content {
+    visibility: hidden;
+    height: 0;
+    overflow: hidden;
+  }
+
+  &--opened {
+    .dropdown__content {
+      visibility: visible;
+      height: auto;
+    }
+
+    .dropdown__icon {
+      transform: translateY(0.1rem);
+    }
+  }
+}
+</style>
